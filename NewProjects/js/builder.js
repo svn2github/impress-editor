bcb8932ae1fd09bcfed63e636a8b10a4d832cc14
filippo.ferrario10@gmail.com <filippo.ferrario10@gmail.com@3da36ee1-3498-3966-3c94-352e8e4c2cfb>
2012-11-29 14:44:52 +0000

    Add two edit button also in new project
    git-svn-id: http://impressjs-wysiwyg-editor.googlecode.com/svn/trunk@38 3da36ee1-3498-3966-3c94-352e8e4c2cfb

--- NewProjects/js/builder.js
+++ NewProjects/js/builder.js
@@ -87,6 +87,9 @@ builder=(function(){
     $('<div></div>').addClass('bt-move').attr('title','Move').data('func','move').appendTo($controls);
     $('<div></div>').addClass('bt-rotate').attr('title','Rotate').data('func','rotate').appendTo($controls);
     $('<div></div>').addClass('bt-scale').attr('title','Scale').data('func','scale').appendTo($controls);
+    $('<div></div>').addClass('bt-rotateXaxial').attr('title','RotateX').data('func','rotateX').appendTo($controls); //added
+    $('<div></div>').addClass('bt-rotateYaxial').attr('title','RotateY').data('func','rotateY').appendTo($controls); //added
+    
     $controls2=$('<div></div>').addClass('builder-controls').hide();
     $('<span></span>').attr('id', 'edit').addClass('builder-bt').text('Edit').appendTo($controls2).click(editContents);
     //$('<span></span>').addClass('builder-bt').text('Wrap').appendTo($controls).click(wrapContents);

3fabdf548e9c30de89865abb792048396cb25b62
hugodife@gmail.com <hugodife@gmail.com@3da36ee1-3498-3966-3c94-352e8e4c2cfb>
2012-12-01 11:48:06 +0000

    added some prevent to set as input letters
    git-svn-id: http://impressjs-wysiwyg-editor.googlecode.com/svn/trunk@44 3da36ee1-3498-3966-3c94-352e8e4c2cfb

--- NewProjects/js/builder.js
+++ NewProjects/js/builder.js
@@ -429,8 +429,8 @@ builder=(function(){
   	
   	//check that is a number
   	//check that the key is enter(checking the value)
-
-  	if(event.keyCode==13){
+	console.log(event.keyCode)
+  	if(event.keyCode==13 && IsNumeric(text)){
   		console.log("1")
   	}
   }
@@ -494,4 +494,27 @@ var rotz=function (event,text){
   		console.log("6")
   	}
   }
+  //Function to prevent the user of setting as input letters
+  $(document).ready(function(){
+  $(".trans").keydown(function(event) {
+        // Allow: backspace, delete, tab, escape, and enter
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)  ||
+            (event.keyCode==189 || event.keyCode==173)
+            
+            ) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if (event.shiftKey ||event.keyCode==109|| (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
 
+});
